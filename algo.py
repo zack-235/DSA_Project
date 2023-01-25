aad5a96a1320adcfecfba1e3dc2814621a34f13a
Abdul Rehman <ar618211@gmail.com>
2023-01-26 02:14:42 +0500

    Selection sort imported and working

--- algo.py
+++ algo.py
@@ -1,25 +1,18 @@
-def selection_sort(list):
+def selection_sort(list1):
 
-    for i in range(len(list)):
+    for i in range(len(list1)):
 
         min=i
 
-        for j in range(i+1,len(list)):
+        for j in range(i+1,len(list1)):
 
-            if list[min]>list[j]:
+            if list1[min]>list1[j]:
                 min=j
 
-        list[i],list[min]=list[min],list[i]
+        list1[i],list1[min]=list1[min],list1[i]
 
-    return list
+    return list1
 
-    
-            
-
-arr=[2,8,6,4,1,3]
-print(selection_sort(arr))
-
-
-
-
-
+if __name__ == '__main__':
+    arr=[2,8,6,4,1,3]
+    print(selection_sort(arr))
--- test.py
+++ test.py
@@ -1,22 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
-import array
+from algo import selection_sort
 
 def on_button_click():
     text = entry.get()
     if text=="":
         label.config(text="Enter only numbers with comma as seperator");
-
     else:
         numbers= text.split(",");
 
-    numbers_arr=array.array("i",);
-    for i in range(numbers.__len__()):
-        numbers_arr[i]=str(numbers[i]);
+    numbers_arr=[];
+    for i in range(len(numbers)):
+        numbers_arr.append(eval(numbers[i]));
+    print(numbers_arr)
 
-    print(number_arr)
-
-    # label.config(text=numbers)
+    sorted_numbers = selection_sort(numbers_arr)
+    label.config(text=sorted_numbers)
 
 root = tk.Tk()
 root.minsize(height=400, width=600);
@@ -30,12 +29,18 @@ entry.grid(row=0, column=0, padx=150, pady=100)
 # entry.config(background="red")
 
 # Create a Button widget
-button = tk.Button(root, text="Sort!", command=on_button_click, width=25)
-button.grid(row=1, column=0, padx=20, pady=20)
+button1 = tk.Button(root, text="Selection Sort!", command=on_button_click, width=25)
+button1.grid(row=2, column=0, padx=20, pady=20)
+
+button2 = tk.Button(root, text="Insertion Sort!", command=on_button_click, width=25)
+button2.grid(row=3, column=0, padx=20, pady=20)
+
+button3 = tk.Button(root, text="Insertion Sort!", command=on_button_click, width=25)
+button3.grid(row=4, column=0, padx=20, pady=20)
 # button.bind("a", on_button_click);
 
 # Create a Label widget
 label = tk.Label(root, text="")
-label.grid(row=2, column=0, columnspan=2, padx=20, pady=20)
+label.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
 
 root.mainloop()
